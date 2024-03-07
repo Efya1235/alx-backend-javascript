11de9e834e306ed30ffd67efb2c2e0beb3fefa7c
Efya1235 <bkumah51@gmail.com>
2024-03-07 04:35:17 -0800

    great

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,23 @@
+import Car from './10-car.js';
+
+class EVCar extends Car {
+    constructor(brand, motor, color, range) {
+        super();
+        this._brand = brand;
+        this._motor = motor;
+        this._color = color;
+        this._range = range;
+    }
+
+    cloneCar() {
+        
+        const car = new Car();
+        car._brand = this._brand;
+        car._motor = this._motor;
+        car._color = this._color;
+        return car;
+    }
+}
+
+export default EVCar;
+
